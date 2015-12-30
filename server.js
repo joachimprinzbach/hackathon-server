34dab81254fc2937a3dd9fab68694ace40b63a21
nick <nick.payne89@gmail.com>
2015-12-30 16:50:56 +0100

    read response from a json file, allow cors, package.json

--- server.js
+++ server.js
@@ -1,17 +1,25 @@
 var express = require('express');
+var fs = require('fs');
 var app = express();
-
-var locations = {};
 
 app.get('/', function (req, res) {
     res.send('Hello World!');
 });
 
-app.get('/locations', function(req, res) {
-   res.json(locations);
+app.all('/', function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
 });
 
-var server = app.listen(3000, function () {
+app.get('/locations', function (req, res) {
+    var locations = JSON.parse(fs.readFileSync('locations.json', 'utf8'));
+    res.json(locations);
+});
+
+app.get('/locations')
+
+var server = app.listen(1337, function () {
     var host = server.address().address;
     var port = server.address().port;
 
